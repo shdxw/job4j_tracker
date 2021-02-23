f9f38a8cb497bcace7a513a5ef2e6e9d00822df7
vlader <Vladarscb@gmail.com>
2021-02-23 19:35:56 +1000

    2. Анализ доступности сервера. [#381624] integration tests

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,45 +1,53 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenOK() {
+    public void whenOK() throws IOException {
         String in = "./src/test/java/ru/job4j/io/un1.csv";
-        String out = "./src/test/java/ru/job4j/io/testOut.csv";
+        File target = folder.newFile("target.txt");
         Analizy analizy = new Analizy();
-        analizy.unavailable(in, out);
+        analizy.unavailable(in, target.getAbsolutePath());
         assertEquals("10:57:01;10:59:01;" + System.lineSeparator() +
-                "11:01:02;11:02:02;", analizy.toString(out));
+                "11:01:02;11:02:02;", analizy.toString(target.getAbsolutePath()));
     }
 
     @Test
-    public void exeptionMore200() {
+    public void exeptionMore200() throws IOException {
         String in = "./src/test/java/ru/job4j/io/un3.csv";
-        String out = "./src/test/java/ru/job4j/io/testOut.csv";
+        File target = folder.newFile("target.txt");
         Analizy analizy = new Analizy();
-        analizy.unavailable(in, out);
+        analizy.unavailable(in, target.getAbsolutePath());
         assertEquals("10:57:01;10:59:01;" + System.lineSeparator() +
-                "11:01:02;11:02:02;", analizy.toString(out));
+                "11:01:02;11:02:02;", analizy.toString(target.getAbsolutePath()));
     }
 
     @Test
-    public void only200() {
+    public void only200() throws IOException {
         String in = "./src/test/java/ru/job4j/io/un4.csv";
-        String out = "./src/test/java/ru/job4j/io/testOut.csv";
+        File target = folder.newFile("target.txt");
         Analizy analizy = new Analizy();
-        analizy.unavailable(in, out);
-        assertEquals("", analizy.toString(out));
+        analizy.unavailable(in, target.getAbsolutePath());
+        assertEquals("", analizy.toString(target.getAbsolutePath()));
     }
 
     @Test
-    public void only400() {
+    public void only400() throws IOException {
         String in = "./src/test/java/ru/job4j/io/un5.csv";
-        String out = "./src/test/java/ru/job4j/io/testOut.csv";
+        File target = folder.newFile("target.txt");
         Analizy analizy = new Analizy();
-        analizy.unavailable(in, out);
-        assertEquals("11:01:02;now;", analizy.toString(out));
+        analizy.unavailable(in, target.getAbsolutePath());
+        assertEquals("11:01:02;now;", analizy.toString(target.getAbsolutePath()));
     }
 }
