841a57fde6a7e41521d5b59fd6a3f455fc7648dc
vlader <Vladarscb@gmail.com>
2021-03-01 15:01:00 +1000

    fix chat

--- src/main/java/ru/job4j/chat/ConsoleChat.java
+++ src/main/java/ru/job4j/chat/ConsoleChat.java
@@ -3,10 +3,8 @@ package ru.job4j.chat;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,10 +16,53 @@ public class ConsoleChat {
     private final String botAnswers;
     private List<String> allAnswers;
 
+    private final Map<String, Function<String, Boolean>> dispatch = new HashMap<>();
+
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
         makeAnswers();
+        init();
+    }
+
+    public Function<String, Boolean> toStop() {
+        return msg -> {
+            addAnswer(msg, "You: ");
+            return true;
+        };
+    }
+
+    public Function<String, Boolean> toOut() {
+        return msg -> {
+            addAnswer(msg, "You: ");
+            return true;
+        };
+    }
+
+    public Function<String, Boolean> toCon() {
+        return msg -> {
+            addAnswer(msg, "You: ");
+            return false;
+        };
+    }
+
+    public void init() {
+        this.load(STOP, this.toStop());
+        this.load(OUT, this.toOut());
+        this.load(CONTINUE, this.toCon());
+    }
+
+    public void load(String type, Function<String, Boolean> handle) {
+        this.dispatch.put(type, handle);
+    }
+
+    public boolean sent(final String msg, boolean botStop) {
+        Function<String, Boolean> function = this.dispatch.get(msg);
+        if (!(function == null)) {
+            return function.apply(msg);
+        }
+        addAnswer(msg, "You: ");
+        return botStop;
     }
 
     private void makeAnswers() {
@@ -38,10 +79,21 @@ public class ConsoleChat {
         }
     }
 
+    private void addAnswer(String msg, String username) {
+        try (BufferedWriter out = new BufferedWriter(
+                new FileWriter(path, StandardCharsets.UTF_8, true))) {
+            out.append(username).append(msg).append(System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private String giveAnswer() {
         Random rand = new Random();
         int len = allAnswers.size();
-        return allAnswers.get((len - 1) - rand.nextInt(len));
+        String rsl = allAnswers.get((len - 1) - rand.nextInt(len));
+        System.out.println(rsl);
+        return rsl;
     }
 
     public void run() {
@@ -49,37 +101,14 @@ public class ConsoleChat {
         System.out.println("Добро пожаловать в чат, поздоровайтесь");
         Scanner scan = new Scanner(System.in);
         boolean stop = false;
-        try (BufferedWriter out = new BufferedWriter(
-                new FileWriter(path, StandardCharsets.UTF_8, true))) {
                 for (; !input.equals(OUT);) {
                     input = scan.nextLine();
                     System.out.println("You: " + input);
-                    out.newLine();
-                    out.append("You: ").append(input);
-                    switch (input) {
-
-                        case(STOP):
-                            stop = true;
-                            break;
-                        case(OUT):
-                            break;
-                        case (CONTINUE):
-                            stop = false;
-                        default:
-                            if (!stop) {
-                                System.out.println("Bot: " + giveAnswer());
-                                out.newLine();
-                                out.append("Bot: ").append(giveAnswer());
-                            }
-                            break;
+                    stop = sent(input, stop);
+                    if (!stop) {
+                        addAnswer(giveAnswer(), "Bot: ");
                     }
                 }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.exit(0);
     }
 
     public static void main(String[] args) {
