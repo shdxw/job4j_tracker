73e1f67002953b351c4507a4993362e578f684dd
vlader <Vladarscb@gmail.com>
2021-03-04 18:55:21 +1000

    6. Кодировка. [#381626]

---
+++ src/main/java/ru/job4j/serilization/Cat.java
@@ -0,0 +1,43 @@
+package ru.job4j.serilization;
+
+import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Cat {
+    private String name;
+    private boolean sterilized;
+    private int age;
+    private String[] children;
+    private Contact contact;
+
+    public Cat(String name, boolean sterilized, int age, String[] children, Contact contact) {
+        this.name = name;
+        this.sterilized = sterilized;
+        this.age = age;
+        this.children = children;
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", sterilized=" + sterilized +
+                ", age=" + age +
+                ", children=" + Arrays.toString(children) +
+                ", contact=" + contact +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat("myao", true, 23, new String[]{"myao2", "myao3"},
+                new Contact(234, "89242640011"));
+        final Gson gson = new GsonBuilder().create();
+        String catJson = gson.toJson(cat); //серилизуем кота в json
+        System.out.println(catJson);
+        final Cat catFromJson = gson.fromJson(catJson, Cat.class); //десериализуем кота
+        System.out.println(cat.toString() + System.lineSeparator() + catFromJson.toString());
+    }
+}
